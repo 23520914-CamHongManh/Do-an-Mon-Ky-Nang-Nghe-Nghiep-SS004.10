bd3a2053e4121399c2610f6ecf78f6bbbbcefb60
23520914-Cam Hong Manh <23520914@gm.uit.edu.vn>
2024-05-04 23:39:58 +0700

    Ghep CONRAN.cpp vao ham main.cpp

--- main.cpp
+++ main.cpp
@@ -29,10 +29,12 @@ public:
     struct Point A[100];
     int DoDai;
     CONRAN(){
-        DoDai = 3;
-        A[0].x = 10; A[0].y = 10;
-        A[1].x = 11; A[1].y = 10;
-        A[2].x = 12; A[2].y = 10;
+        DoDai = 4;
+        for(int i=0;i<DoDai-1;i++)
+        {
+            A[i].x=tuongtrai+9-i;
+            A[i].y=15;
+        }
     }
     void Ve(){
         for (int i = 0; i < DoDai; i++){
@@ -50,10 +52,39 @@ public:
     void DiChuyen(int Huong){
         for (int i = DoDai-1; i>0;i--)
             A[i] = A[i-1];
-        if (Huong==0) A[0].x = A[0].x + 1;
-        if (Huong==1) A[0].y = A[0].y + 1;
-        if (Huong==2) A[0].x = A[0].x - 1;
-        if (Huong==3) A[0].y = A[0].y - 1;
+            switch (Huong)
+        {
+        case 2:
+            A[0].x--; break;
+        case 1:
+            A[0].y++; break;
+        case 0:
+            A[0].x++; break;
+        case 3:
+            A[0].y--; break;
+        }
+    }
+    void XoaCu()
+    {
+        gotoxy(A[DoDai].x,A[DoDai].y);
+        cout<<" ";
+    }
+
+    bool Game_Over2()
+    {
+    for( int i=1; i < DoDai ; i++)
+    {
+        if(( A[0].x== A[i].x)&& (A[0].y == A[i].y))
+            return true;
+    }
+    return false;
+
+    }
+    bool Game_Over1()
+    {   if(A[0].x==tuongtrai ||A[0].x== tuongphai ||A[0].y==tuongtren ||A[0].y==tuongduoi)
+        {return true;}
+
+     return false;
     }
 };
 void Ve_Tuong();
