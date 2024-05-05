614200771cc012ff6534133957c909b9a676de9b
23520914-Cam Hong Manh <23520914@gm.uit.edu.vn>
2024-05-05 21:25:23 +0700

    Add ham Gameplay2 cho che do choi 2

---
+++ Gameplay2.cpp
@@ -0,0 +1,30 @@
+void Gameplay2(int mucdo)
+{
+    CONRAN r;
+    QUA qua;
+    int Huong = 0;
+    char t;
+    Ve_Tuong();
+    TaoQua(qua,r);
+    qua.Ve();
+  while (1){
+        r.XoaCu();
+        r.Ve();
+        if(Game_Over1(r))
+        {
+            gotoxy(tuongtrai+25,tuongduoi-10);
+            cout<<"BAN DA CHET";
+            break;
+        }
+        if (kbhit()){
+            t = getch();
+            if (t=='a'&& Huong!=0) Huong = 2;
+            if (t=='w'&& Huong!=1) Huong = 3;
+            if (t=='d'&& Huong!=2) Huong = 0;
+            if (t=='s'&& Huong!=3) Huong = 1;
+        }
+        AnQua(qua,r,DIEM);
+        r.DiChuyen(Huong);
+        Sleep(mucdo);
+    }
+}
