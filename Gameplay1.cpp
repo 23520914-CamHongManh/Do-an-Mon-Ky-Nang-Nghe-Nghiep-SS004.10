f85eb7c566e52dc7a3a75917d7ca96e372dd6ae9
23520914-Cam Hong Manh <23520914@gm.uit.edu.vn>
2024-05-05 19:53:41 +0700

    Cap nhat them ham Gameplay1

--- Gameplay1.cpp
+++ Gameplay1.cpp
@@ -1,12 +1,21 @@
-void Gameplay1()
+void Gameplay1(int mucdo)
 {
     CONRAN r;
     QUA qua;
     int Huong = 0;
     char t;
+    Ve_Tuong();
+    TaoQua(qua,r);
+    qua.Ve();
   while (1){
         r.XoaCu();
         r.Ve();
+        if(Game_Over1(r))
+        {
+            gotoxy(tuongtrai+25,tuongduoi-10);
+            cout<<"BAN DA CHET";
+            break;
+        }
         if (kbhit()){
             t = getch();
             if (t=='a'&& Huong!=0) Huong = 2;
@@ -14,7 +23,8 @@ void Gameplay1()
             if (t=='d'&& Huong!=2) Huong = 0;
             if (t=='s'&& Huong!=3) Huong = 1;
         }
+        AnQua(qua,r,DIEM);
         r.DiChuyen(Huong);
-        Sleep(300);
+        Sleep(mucdo);
     }
 }
